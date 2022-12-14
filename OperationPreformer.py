707f7986198de49fbe02fd3c7a372b1a3e363218
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2022-12-14 16:24:33 +0200

    Updated the OperationPreformen file: Added a dictionary to match the operator to its check function and its operation function. This module is in charge of checking and preforming any mathematical operation supported by the calculator.

---
+++ OperationPreformer.py
@@ -0,0 +1,40 @@
+from MathematicsOperations import *
+from OperationCheck import *
+
+"""
+Module to preform the mathematical operations supported by the calculator.
+It matches the operator to its function, checks that it can be preformed, and preforms it if possible.
+"""
+
+# dictionary of all the calculation functions and the check function, for each operator
+OPERATORS_FUNCS = {"+": (add, check_binary),
+                   "-": (sub, check_binary),
+                   "*": (mult, check_binary),
+                   "/": (div, check_div_modulo),
+                   "^": (power, check_power),
+                   "%": (modulo, check_div_modulo),
+                   "$": (maximum, check_binary),
+                   "&": (minimum, check_binary),
+                   "@": (avg, check_binary),
+                   "~": (neg, check_unary),
+                   "!": (factorial, check_factorial),
+                   "#": (sum_digits, check_unary)
+                   }
+
+
+def preform_operation(operator: str, index, *operands) -> float:
+    """
+    The function gets an operator and its matching operands, and preforms it.
+    :param operator: operator
+    :param index: index of the operator in the equation
+    :param operands: matching operands
+    :return:
+    """
+    check_operation(OPERATORS_FUNCS[operator][1], operator, index, *operands)
+
+    # if too many recursions or overflow, the result is so high that the calculation is too much.
+    # in this case, the result is inf (infinity)
+    try:
+        return OPERATORS_FUNCS[operator][0](*operands)
+    except (RecursionError, OverflowError):
+        return float("inf")
