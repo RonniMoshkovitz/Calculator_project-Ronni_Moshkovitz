0c322cfc0e3e48bd6bbcc4ed60ff2c5091241e76
RonniMoshkovitz <117098162+RonniMoshkovitz@users.noreply.github.com>
2022-12-14 15:46:21 +0200

    Updated the OperationCheck file: Added Functions to check each operation before execution (checks operands validity). The main function of this module gets the check function to preform and the equation variables to check, it raises an exception if the validation failed.

---
+++ OperationCheck.py
@@ -0,0 +1,96 @@
+from CalculatorExceptions import EquationMathError
+
+"""
+Module containing all the operation validations (checks) for the supported operations on the calculator.
+The main function (check_operations preforms the validation according to the given parameters,
+and raises exceptions if needed.
+"""
+
+
+def check_binary(operand_a: float, operand_b: float) -> str or None:
+    """
+    The function checks if the operands given are operands.
+    :param operand_a: first operand
+    :param operand_b: second operand
+    :return: None if the operands are valid, otherwise the exception code of missing operand.
+    """
+    if not (is_operand(operand_a) and is_operand(operand_b)):
+        return "MON"
+
+
+def check_unary(operand: float) -> str or None:
+    """
+    The function checks if the operand given is an operands.
+    :param operand: operand
+    :return: None if the operands are valid, otherwise the exception code of missing operand.
+    """
+    if not is_operand(operand):
+        return "MON"
+
+
+def check_div_modulo(operand_a: float, operand_b: float) -> str or None:
+    """
+    The function checks if the operands are valid for division.
+    :param operand_a: first operand
+    :param operand_b: second operand
+    :return: None if the operands are valid, otherwise the exception code of the invalidity.
+    """
+    # checks if the operands are operands
+    failed = check_binary(operand_a, operand_b)
+
+    if not failed and operand_b == 0:
+        return "ZD"
+    return failed
+
+
+def check_power(operand_a: float, operand_b: float) -> str or None:
+    """
+    The function checks if the operands are valid for power.
+    :param operand_a: first operand
+    :param operand_b: second operand
+    :return: None if the operands are valid, otherwise the exception code of the invalidity.
+    """
+    # checks if the operands are operands
+    failed = check_binary(operand_a, operand_b)
+
+    if not failed and (operand_a <= 0 and 0 < operand_b < 1):
+        return "PE"
+    return failed
+
+
+def check_factorial(operand: float) -> str or None:
+    """
+    The function checks if the operands are valid for factorial.
+    :param operand: operand
+    :return: None if the operands are valid, otherwise the exception code of the invalidity.
+    """
+    # checks if the operands are operands
+    failed = check_unary(operand)
+
+    if not failed and (operand <= 0 or not operand.is_integer()):
+        return "FE"
+    return failed
+
+
+def check_operation(check_func, operator: str, index, *operands: float or str):
+    """
+    The function checks the validity of the operands according to the check function.
+    If invalid, raises exception accordingly.
+    :param check_func: check function to activate
+    :param operator: the operator
+    :param index: index of the operation in the equation (for the informative exception)
+    :param operands: operands to validate
+    """
+    # checks if invalid
+    exception_code = check_func(*operands)
+    if exception_code:
+        raise EquationMathError(exception_code, index, f"\n\t --> for {operator}")
+
+
+def is_operand(operand: float) -> bool:
+    """
+    The function checks if the given operand is a number (valid operand).
+    :param operand: operand
+    :return: True if valid, False otherwise
+    """
+    return type(operand) is float
